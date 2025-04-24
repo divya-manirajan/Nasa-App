9b66e92f849485ac0cda4535e53564cfc5ec557a
divya-manirajan <divya.manirajan@gmail.com>
2025-04-24 16:46:32 -0500

    updated main page mars section

--- app/mars/page.js
+++ app/mars/page.js
@@ -1,11 +1,32 @@
-
-
+'use client'
 export default function page() {
-  return (
-    <div>
 
-        MARS
-      
+  // https://api.nasa.gov/mars-photos/api/v1/rovers/{rover_name}/photos?{params}&api_key=xkWAoktdm19dskmAsmgiDoxR8kPsIeRwRfaGC1xV
+
+  // 1 - Style main page and form div
+  // 2 - Write out form to keep track of all params as variables
+  // 3 - Fetch relevant data from API
+  // 4 - Desplay Data in Gallery View (img and description)
+  // 5 - Have Detailed View for each (all relevant info)
+
+ 
+  return (
+    <div> {/* Style out background of page */}
+      <form>
+        {/* Build out form with main params ++ sytle */}
+      </form>
+      {/* params = sol or earth-date, camera, (page - optional), api_key, rover_name (Curiosity,	Opportunity,	Spirit) */}
+      {/* Cameras: FHAZ, RHAZ, MAST, CHECMCAM, MAHLI, MARDI, NAVCAM, PANCAM, MINITES */}
+      {/* Abbreviation	        Camera
+          FHAZ	           Front Hazard Avoidance Camera
+          RHAZ	           Rear Hazard Avoidance Camera
+          MAST	           Mast Camera
+          CHEMCAM	         Chemistry and Camera Complex
+          MAHLI	           Mars Hand Lens Imager
+          MARDI	           Mars Descent Imager
+          NAVCAM	         Navigation Camera
+          PANCAM	         Panoramic Camera
+          MINITES	         Miniature Thermal Emission Spectrometer (Mini-TES) */}
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -7,25 +7,54 @@ export default function page() {
   const router = useRouter()
   const currentDate = new Date().toJSON().slice(0, 10);
 
+  const prevDate = new Date(currentDate)
+  prevDate.setDate(prevDate.getDate() - 1)
+  const formattedPrevDate = prevDate.toJSON().slice(0, 10);
 
   const [APOD_IMG, setAPOD_IMG] = useState(null)
+  const [MARS_IMG, setMARS_IMG] = useState(null)
 
 
-  const getImages = async () => {
+  const getAPODIMG = async () => {
     try{
       const APOD_API = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
 
-      const response = await fetch(APOD_API)
-      const data = await response.json()
-      console.log(data.url)
-      setAPOD_IMG(data.url)
+      const APOD_response = await fetch(APOD_API)
+      const APOD_data = await APOD_response.json()
+      console.log(APOD_data.url)
+      setAPOD_IMG(APOD_data.url)
+
     } catch (e) {
       console.log(e)
     }    
   }
 
+  const getMARSIMG = async () => {
+    try{   
+      const MARS_API = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${currentDate}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+      
+      let MARS_response = await fetch(MARS_API)
+      let MARS_data = await MARS_response.json()
+      console.log(MARS_data.photos.length)
+
+      if(MARS_data.photos.length === 0){
+        const MARS_API = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${formattedPrevDate}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+      
+        MARS_response = await fetch(MARS_API)
+        MARS_data = await MARS_response.json()
+        console.log(MARS_data)
+      }
+      console.log(MARS_data)
+      setMARS_IMG(MARS_data) 
+    } catch (e) {
+      console.log(e)
+    } 
+  }
+
+
   useEffect(() => {
-    getImages()
+    getAPODIMG()
+    getMARSIMG()
   } , [])
 
 //Home Screen turn into basic title screen
@@ -35,7 +64,7 @@ export default function page() {
       <div className="h-full w-[800px] text-center">
         <h1 className="mb-5 text-xl text-white ">Welcome to Cosmic View</h1>
         <div className="grid grid-cols-2 grid-rows-4 gap-4">
-          <div className="bg-blue-100 row-span-2 flex flex-col items-center p-4 rounded-3xl bg-radial from-blue-950 to-sky-700 shadow-md shadow-cyan-500/50 hover:shadow-inner cursor-pointer" onClick={() => router.push(`/apod?start=${currentDate}&end=${currentDate}`)}>
+          <div className="max-h-fit bg-blue-100 row-span-2 flex flex-col items-center p-4 rounded-3xl bg-radial from-blue-950 to-sky-700 shadow-md shadow-cyan-500/50 hover:shadow-inner cursor-pointer" onClick={() => router.push(`/apod?start=${currentDate}&end=${currentDate}`)}>
             <h1 className="text-lg mb-2">Astronomy Picture of the Day (APOD)</h1>
             {APOD_IMG && <img className="h-48" src={APOD_IMG} />}
             <p className="mt-2 text-sm">Astronomy Picture of the Day (APOD) is a website provided by 
@@ -46,7 +75,7 @@ export default function page() {
           </div>
           <div className="bg-blue-100 col-start-2 row-start-2 row-span-2 flex flex-col items-center p-4 rounded-3xl bg-radial from-blue-950 to-sky-700 shadow-md shadow-cyan-500/50">
             <h1 className="text-lg mb-2">Mars Rover Photos</h1>
-            {/* <img className="h-48" src="/vercel.svg" /> */}
+            {MARS_IMG && <img src={MARS_IMG.photos[0].img_src}></img>}
             <p className="bg-black ">Coming Soon</p>
             <p className="mt-2 text-sm">This API is designed to collect image data gathered by NASA's 
               Curiosity, Opportunity, and Spirit rovers on Mars and make it 
